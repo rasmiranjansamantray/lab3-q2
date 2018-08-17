f5181ba6cff69e2e5a8e27a035c3d36c1bbc8104
rasmiranjansamantray <42238972+rasmiranjansamantray@users.noreply.github.com>
2018-08-17 12:26:35 +0530

    Add files via upload

---
+++ lab3/lab3_q2.cpp
@@ -0,0 +1,21 @@
+// library
+#include<iostream>
+using namespace std;
+int main()
+//process
+{
+//declaring variables
+int a=1;
+char b='2';
+float c=3.004;
+double d=5.06;
+bool e=1;
+//will get size of variables
+cout <<"size of int:"<<sizeof(int)<<"byte"<<" contents of a="<<a<<endl;
+cout <<"size of char:"<<sizeof(char)<<"byte"<<" contents of b="<<b<<endl;
+cout <<"size of float:"<<sizeof(float)<<"byte"<<" contents of c="<<c<<endl;
+cout <<"size of double:"<<sizeof(double)<<"byte"<<" contents of d="<<d<<endl;
+cout <<"size of bool:"<<sizeof(bool)<<"byte"<<" contents of e="<<e<<endl;
+return 1;
+}
+
